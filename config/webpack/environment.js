d7eac213650513324709c9220af38361397c480b
Julian Rubisch <julian@julianrubisch.at>
2019-11-05 08:33:09 +0100

    Initial commit

---
+++ config/webpack/environment.js
@@ -0,0 +1,11 @@
+const { environment } = require('@rails/webpacker')
+
+// const babelLoader = environment.loaders.get('babel');
+// console.log(babelLoader.use[0]);
+// babelLoader.use[0].options = {
+//   cacheDirectory: 'tmp/cache/webpacker/babel-loader',
+//   presets: ['env'],
+//   plugins: ['transform-object-rest-spread']
+// }
+
+module.exports = environment
